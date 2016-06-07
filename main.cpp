6eb2f9932694730bee4cf16847ddf34a23b6e267
Krzysztof Szpieg <kszpieg@gmail.com>
2016-06-07 20:38:57 +0200

    v 1.0
    DO OCENY

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 #include <iostream>
 
+
 using namespace std;
 int main (int argc, char const *argv[])
 {
 	int repeats = atoi(argv[1]);
 	elevator test;
+	cout << "\nWitaj w programie ' Symulator Windy ' v1.0. \nINFO: Winda bedzie sobie jezdzic na zasadzie gora-dol\nRozpocznijmy symulacje! \n" << endl;
 	for (int i=0; i<repeats; i++)
 	{	
 		cout << "############### " << i+1 << ". TURA ###############" << endl;
